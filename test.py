417f907ca914a43f23ca1df5cf38147c43f2ec8c
Pedro Aureliano <84044726+eksdat@users.noreply.github.com>
2026-04-08 17:21:06 -0300

    remoção do test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from app.github import extract_repo_info
-from app.analyzer import analyze_repo
-import json
-
-repo_data = extract_repo_info("https://github.com/eksdat/mlp-neural-network")
-analise = analyze_repo(repo_data)
-
-print(json.dumps(analise, indent=2, ensure_ascii=False))
